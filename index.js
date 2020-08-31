ac8ef2b54d87817065b31ed0a6513c679546bbf4
A00079 <bajay0007@gmail.com>
2020-09-01 02:54:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 5000;
+const SubmitSerway = require('./routes/UserSerway/submitSerway.js');
+
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+// parse application/vnd.api+json as json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+// Middlewares
+app.use(cors());
+// Routes
+app.use('/api/user/submitserway/create', SubmitSerway);
+
+if (process.env.NODE_ENV === 'production') {
+
+    app.use(express.static(path.join(__dirname, 'build')));
+    app.get(/.*/, (req, res) => {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    });
+}
+
+app.listen(port, () => {
+    console.log(`Server Running at ${port}`);
+});
