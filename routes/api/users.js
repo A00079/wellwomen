b728d588a2201a4cb80bc85d8a614672722d476a
Ajay 007 <ajaybendre@credenceanalytics.com>
2020-09-03 04:26:27 +0530

    email fix

--- routes/api/users.js
+++ routes/api/users.js
@@ -124,35 +124,3 @@ router.post("/login", (req, res) => {
 });
 
 module.exports = router;
-
-// The callback that is invoked when the user visits the confirmation
-// url on the client and a fetch request is sent in componentDidMount.
-module.exports.confirmEmail = (req, res) => {
-  const { id } = req.params
-
-  User.findById(id)
-    .then(user => {
-
-      // A user with that id does not exist in the DB. Perhaps some tricky 
-      // user tried to go to a different url than the one provided in the 
-      // confirmation email.
-      if (!user) {
-        res.json({ msg: msgs.couldNotFind })
-      }
-      
-      // The user exists but has not been confirmed. We need to confirm this 
-      // user and let them know their email address has been confirmed.
-      else if (user && !user.confirmed) {
-        User.findByIdAndUpdate(id, { confirmed: true })
-          .then(() => res.json({ msg: msgs.confirmed }))
-          .catch(err => console.log(err))
-      }
-
-      // The user has already confirmed this email address.
-      else  {
-        res.json({ msg: msgs.alreadyConfirmed })
-      }
-
-    })
-    .catch(err => console.log(err))
-}
