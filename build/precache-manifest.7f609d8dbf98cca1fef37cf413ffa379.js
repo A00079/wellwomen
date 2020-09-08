607f4002a4388f19c7e2146ea70e2d7739e4a129
Ajay 007 <ajaybendre@credenceanalytics.com>
2020-09-08 15:42:36 +0530

    UI fix

---
+++ build/precache-manifest.7f609d8dbf98cca1fef37cf413ffa379.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74d422021377ff9ceb6d80ad9c47866c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07ffec845e1ef3dd989a",
+    "url": "/static/css/2.8f118048.chunk.css"
+  },
+  {
+    "revision": "54e89f2b9ea74baf5c82",
+    "url": "/static/css/main.c2090167.chunk.css"
+  },
+  {
+    "revision": "07ffec845e1ef3dd989a",
+    "url": "/static/js/2.adcdbb1b.chunk.js"
+  },
+  {
+    "revision": "8273986cd85177be9445df20a76c7f68",
+    "url": "/static/js/2.adcdbb1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54e89f2b9ea74baf5c82",
+    "url": "/static/js/main.f5997130.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "e52419719dd0c4c8b172136f0b625008",
+    "url": "/static/media/bg.e5241971.jpg"
+  },
+  {
+    "revision": "843781834b1db2908f08d921c6460c81",
+    "url": "/static/media/bg2.84378183.jpg"
+  },
+  {
+    "revision": "e76de24b6d8ab5e920bb4d49241ad15f",
+    "url": "/static/media/bg3.e76de24b.jpg"
+  },
+  {
+    "revision": "199e9ec14308832c6a8a49bd96c1fde6",
+    "url": "/static/media/bg4.199e9ec1.jpg"
+  },
+  {
+    "revision": "630f0b4ad11765440ca65ba1e4657ef8",
+    "url": "/static/media/downloads.630f0b4a.png"
+  },
+  {
+    "revision": "fc45d717e648a2c2a83b835df71d9a8e",
+    "url": "/static/media/mainvideo.fc45d717.mp4"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fc402b6c10e706c587c93d8e79506792.js"
+  "/precache-manifest.7f609d8dbf98cca1fef37cf413ffa379.js"
 );
 
 self.addEventListener('message', (event) => {
