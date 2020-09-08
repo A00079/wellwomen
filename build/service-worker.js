04693b069e2e10d176a2979d43cbab6865a86c82
Ajay 007 <ajaybendre@credenceanalytics.com>
2020-09-08 16:10:07 +0530

    fix ui

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.61163838751e92789288a59952698279.js"
+  "/precache-manifest.7d9ea40e546f46d1e5635eff171b82dc.js"
 );
 
 self.addEventListener('message', (event) => {
