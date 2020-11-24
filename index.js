581c5de18dafd29e4f89775c0bfd51bf0cfe804d
A00079 <bajay0007@gmail.com>
2020-11-24 22:59:06 +0530

    portal

--- index.js
+++ index.js
@@ -86,13 +86,13 @@ app.use("/api/users", users);
 
 if (process.env.NODE_ENV === 'production') {
 
-    app.use(express.static(path.join(__dirname, 'build')));
-    app.get(/.*/, (req, res) => {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'));
-	});
+    // app.use(express.static(path.join(__dirname, 'build')));
+    // app.get(/.*/, (req, res) => {
+    //     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+	// });
 
     app.use(express.static(path.join(__dirname, 'portal/build')));
-	app.get('/supportportal', (req, res) => {
+	app.get(/.*/, (req, res) => {
         res.sendFile(path.join(__dirname, 'portal/build', 'index.html'));
     });
 }
