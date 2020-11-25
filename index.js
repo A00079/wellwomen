dac8d391cb3e5c8f740fcadcce9467f89d89e659
A00079 <bajay0007@gmail.com>
2020-11-25 22:47:49 +0530

    bug fix

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ app.use("/api/users", users);
 if (process.env.NODE_ENV === 'production') {
 
 	app.use(express.static(path.join(__dirname, 'client/build')));
-	app.get(/.*/, (req, res) => {
+	app.get('/*', (req, res) => {
 		res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 	});
 	
